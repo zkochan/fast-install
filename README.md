c141db2f33533f4c0886a2674871deeb1cfe72a9
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-07-12 22:18:41 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# fast-install
+
+> Installs npm dependencies. When possible, uses pnpm
+
+## Installation
+
+```sh
+npm install --save fast-install
+```
+
+## Usage
+
+```js
+var install = require('fast-install')
+
+install()
+```
+
+## License
+
+[MIT](./LICENSE) © [Zoltan Kochan](http://kochan.io)
---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = install
+
+var spawn = require('cross-spawn')
+
+function install (opts) {
+  opts = opts || {}
+  opts.dependencies = opts.dependencies || []
+  return spawn.sync(getInstaller(), ['install', opts.cliOptions || ''].concat(opts.dependencies), {
+    cwd: opts.cwd,
+    stdio: 'inherit',
+    env: process.env,
+  })
+}
+
+function getInstaller () {
+  var result = spawn.sync('pnpm', ['-v'])
+  return result.status === 0 ? 'pnpm' : 'npm'
+}
